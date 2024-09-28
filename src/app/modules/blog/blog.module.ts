3fe58ad53550a7de5dedca58fb00fb7c048fa884
Vectorfield4 <vectorfield4@gmail.com>
2024-09-28 03:09:37 +0300

    blog module

---
+++ src/app/modules/blog/blog.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const blogRoutes = [];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlogModule { }
